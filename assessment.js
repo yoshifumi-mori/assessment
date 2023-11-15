a4328936759e06e7078b427b3b25fb2b71562a55
yoshifumi-mori <150756662+yoshifumi-mori@users.noreply.github.com>
2023-11-14 18:05:44 -0700

    Bootstrap練習

--- assessment.js
+++ assessment.js
@@ -16,14 +16,27 @@ assessmentButton.onclick =()=>{
 
   //診断結果エリアの作成
   resultDivision.innerText ='';
-  const header =document.createElement('h3');
-  header.innerText='診断結果';
-  resultDivision.appendChild(header);
+  //headerDivisionの作成
+  const headerDivision =document.createElement('div');
+  headerDivision.setAttribute('class', 'card-header text-bg-primary');
+  headerDivision.innerText='診断結果';
+  
+  //bodyDivisionの作成
+  const bodyDivision = document.createElement('div');
+  bodyDivision.setAttribute('class','card-body');
 
   const paragraph = document.createElement('p');
+  paragraph.setAttribute('class', 'card-text');
   const result = assessment(userName);
   paragraph.innerText=result;
-  resultDivision.appendChild(paragraph);
+  bodyDivision.appendChild(paragraph);
+
+  //resultDivisionにBootstarpのスタイルを適用する
+  resultDivision.setAttribute('class', 'card');
+ 
+  //headerDivisionとbodyDivisionをresultDivisionに差し込む
+  resultDivision.appendChild(headerDivision);
+  resultDivision.appendChild(bodyDivision);
 
   //TODOツイートエリアの作成
   tweetDivision.innerText = '';
